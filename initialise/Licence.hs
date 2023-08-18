03bb60a78a8c499cae2b77fd3a825fbf389e2d12
Alex Brandt <alunduil@gmail.com>
2023-08-18 20:44:40 +0000

    Checkpoint licence extraction.

--- initialise/Licence.hs
+++ initialise/Licence.hs
@@ -2,7 +2,7 @@ module Licence (text) where
 
 import Configuration (MetaData (name))
 import Control.Monad.Extra (pureIf)
-import Data.ByteString.Lazy (ByteString, empty)
+import Data.ByteString.Lazy (ByteString)
 import Data.Maybe (fromJust)
 import Network.HTTP.Client (responseBody)
 import Network.HTTP.Simple (httpLBS, parseRequest, setRequestResponseTimeout)
@@ -14,11 +14,14 @@ text name = fromJust . extract <$> download name
 
 download :: String -> IO ByteString
 download name = do
-  request <- setRequestResponseTimeout 60 <$> parseRequest $ "https://opensource.org/licenses/" ++ escapeURIString isUnescapedInURI name
+  request <-
+    setRequestResponseTimeout 60
+      <$> parseRequest
+      $ "https://opensource.org/licenses/" ++ escapeURIString isUnescapedInURI name
   responseBody <$> httpLBS request
 
 extract :: ByteString -> Maybe ByteString
-extract html = pureIf (not empty result) $ head result
+extract html = pureIf (not $ null result) $ head result
   where
     tags = parseTags html
     selector t = (t ~/= "<div id=LicenseText>") || (t ~/= "<article>")
