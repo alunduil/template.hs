90e551c4308489f55190450535e572a18dd8ee64
Alex Brandt <alunduil@gmail.com>
2023-08-29 19:31:53 +0000

    Checkpoint Configuration tests.

--- test/initialise/ConfigurationSpec.hs
+++ test/initialise/ConfigurationSpec.hs
@@ -6,31 +6,30 @@ import qualified Configuration as SUT
 import Data.Maybe (fromJust)
 import Defaults (Defaults (..))
 import Network.URI (parseURI)
-import Options.Applicative (defaultPrefs, execParserPure, helper, info, renderFailure, (<**>))
-import Test.Hspec (Spec, describe, it, shouldBe, shouldNotBe)
+import Options.Applicative (ParserFailure, ParserHelp, ParserResult (CompletionInvoked, Failure, Success), defaultPrefs, execParserPure, failureCode, helper, info, renderFailure, (<**>))
+import System.Exit (ExitCode (ExitFailure, ExitSuccess))
+import Test.Hspec (Expectation, Spec, describe, expectationFailure, it, shouldBe, shouldNotBe)
 
 spec :: Spec
 spec =
   describe "Configuration" $ do
     describe "parser" $ do
-      it "should error if homepage isn't a URI" $ do
-        (error, exitCode) <-
-          renderFailure
-            <$> execParserPure
-              defaultPrefs
-              (info (SUT.parser defaults <**> helper))
-              ["--homepage", "not-a-url"]
-        exitCode `shouldNotBe` 0
-        error `shouldBe` ""
+      it "should error if homepage isn't a URI" $
+        parse ["--homepage", "not-a-url"] `shouldFailWith` ("", ExitFailure 1)
       it "should error if licence isn't an SPDX licence ID" $ do
-        (error, exitCode) <-
-          renderFailure
-            <$> execParserPure
-              defaultPrefs
-              (info (SUT.parser defaults <**> helper))
-              ["--licence", "not-a-licence"]
-        exitCode `shouldNotBe` 0
-        error `shouldBe` ""
+        parse ["--licence", "not-a-licence"] `shouldFailWith` ("", ExitFailure 1)
+
+parse :: [String] -> ParserResult SUT.Configuration
+parse =
+  execParserPure
+    defaultPrefs
+    ( info (SUT.parser defaults <**> helper)
+    )
+
+shouldFailWith :: ParserResult SUT.Configuration -> (String, ExitCode) -> Expectation
+shouldFailWith (Success _) _ = expectationFailure "Expected Failure but got Success"
+shouldFailWith (CompletionInvoked _) _ = expectationFailure "Expected Failure but got CompletionInvoked"
+shouldFailWith (Failure f) rhs = renderFailure f "" `shouldBe` rhs
 
 defaults :: Defaults
 defaults =
