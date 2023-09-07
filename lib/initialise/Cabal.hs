2ca418655827e0c89b05b587f20633b45a59925e
Alex Brandt <alunduil@gmail.com>
2023-09-07 19:50:36 +0000

    Working Cabal file converter.

--- lib/initialise/Cabal.hs
+++ lib/initialise/Cabal.hs
@@ -1,18 +1,19 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
 {-# OPTIONS_GHC -Wno-orphans #-}
 
 module Cabal (replace, convert) where
 
-import Configuration (Configuration (author, licence, name, year))
+import Configuration (Configuration (..))
 import Control.Exception (Exception)
 import Control.Monad.Catch (throwM)
 import Control.Monad.Reader (asks, liftIO)
-import Data.ByteString (ByteString, readFile)
+import Data.ByteString (ByteString, append, breakSubstring, concat, readFile, stripPrefix)
 import Data.ByteString.Char8 (pack)
-import Data.Text (unpack)
+import Data.Text (Text, unpack)
 import Data.Text.Encoding (encodeUtf8)
-import Distribution.Fields (CommentPosition (NoComment), Field (Field, Section), FieldLine (FieldLine), Name (Name), fromParsecFields, readFields, showFields)
+import Distribution.Fields (CommentPosition (NoComment), Field (Field, Section), FieldLine (FieldLine), Name (Name), SectionArg (SecArgName, SecArgStr), fromParsecFields, readFields, showFields)
 import Distribution.Fields.Field (fieldLineAnn)
 import Distribution.Parsec.Position (Position)
 import Distribution.SPDX (licenseId)
@@ -20,7 +21,7 @@ import Initialise (Initialise)
 import System.Directory.Extra (removeFile)
 import System.FilePath (replaceBaseName)
 import Text.Parsec.Error (ParseError)
-import Prelude hiding (readFile)
+import Prelude hiding (concat, readFile)
 
 instance Exception ParseError
 
@@ -42,68 +43,49 @@ convert contents = do
 
 convert' :: Field Position -> Initialise (Field Position)
 convert' f@(Field n@(Name _ fName) ls) = do
-  name' <- asks name
-  licence' <- asks licence
-  author' <- asks author
-  year' <- asks year
-  let annotation = fieldLineAnn . head $ ls
+  Configuration {..} <- asks id
   case fName of
-    "name" -> pure $ Field n [FieldLine annotation (encodeUtf8 name')]
-    "version" -> pure $ Field n [FieldLine annotation "0.1.0.0"]
-    "license" -> pure $ Field n [FieldLine annotation (pack $ licenseId licence')]
-    "copyright" -> pure $ Field n [FieldLine annotation (unwords ["(c)", author', show year'])]
+    -- package
+    "name" -> field (encodeUtf8 name)
+    "version" -> field "0.1.0.0"
+    "license" -> field (pack $ licenseId licence)
+    "copyright" -> field (pack $ unwords ["(c)", show year, unpack author])
+    "author" -> field (encodeUtf8 author)
+    "maintainer" -> field (encodeUtf8 maintainer)
+    "homepage" -> field $ pack $ show homepage
+    "bug-reports" -> field $ pack (show homepage ++ "/issues")
+    "synopsis" -> field "TODO"
+    "description" -> field "TODO"
+    -- common
+    "import" -> field (encodeUtf8 name `append` "-common")
+    "exposed-modules" -> field ""
+    "other-modules" -> field ""
+    "build-depends" -> pure $ Field n $ map (convertFieldLine name) ls
+    "hs-source-dirs" -> pure $ Field n $ map (convertFieldLine name) ls
+    -- source-repository
+    "location" -> field $ pack $ show homepage
     _ -> pure f
-convert' f@(Section n@(Name _ fName) arguments ls) = undefined
+  where
+    field s = pure $ Field n [FieldLine annotation s]
+    annotation = fieldLineAnn . head $ ls
+convert' (Section n arguments fs) = do
+  Configuration {..} <- asks id
+  fs' <- mapM convert' fs
+  pure $ Section n (map (convertSectionArgument name) arguments) fs'
 
-{-
-instance ModifyWith PD.PackageDescription where
-  modifyWith metadata pd =
-    pd
-      { PD.package = modifyWith metadata pd.package,
-        PD.licenseRaw = Left . License . simpleLicenseExpression $ metadata.licence,
-        PD.copyright = toShortText $ unwords ["(c)", show metadata.year, metadata.author],
-        PD.maintainer = toShortText metadata.maintainer,
-        PD.author = toShortText metadata.author,
-        PD.homepage = toShortText $ show metadata.homepage,
-        PD.bugReports = toShortText $ show metadata.homepage ++ "/issues",
-        PD.sourceRepos =
-          [ (emptySourceRepo RepoHead)
-              { repoType = Just $ KnownRepoType Git,
-                repoLocation = Just $ show metadata.homepage
-              }
-          ],
-        PD.executables = modifyWith metadata <$> pd.executables,
-        PD.testSuites = modifyWith metadata <$> pd.testSuites
-      }
+convertSectionArgument :: Text -> SectionArg Position -> SectionArg Position
+convertSectionArgument n s = case s of
+  (SecArgName a o) -> SecArgName a $ convertString n o
+  (SecArgStr a o) -> SecArgStr a $ convertString n o
+  _ -> s
 
-instance ModifyWith PackageIdentifier where
-  modifyWith metadata _pi =
-    PackageIdentifier
-      { pkgName = mkPackageName $ name metadata,
-        pkgVersion = mkVersion [0, 1, 0, 0]
-      }
+convertFieldLine :: Text -> FieldLine Position -> FieldLine Position
+convertFieldLine r (FieldLine annotation s) = FieldLine annotation $ convertString r s
 
-instance ModifyWith Executable where
-  modifyWith metadata executable =
-    executable
-      { exeName = mkUnqualComponentName metadata.name,
-        buildInfo = modifyWith metadata executable.buildInfo
-      }
-
-instance ModifyWith TestSuite where
-  modifyWith metadata testSuite =
-    testSuite
-      { testName = mkUnqualComponentName $ metadata.name ++ "-test",
-        testBuildInfo = modifyWith metadata testSuite.testBuildInfo
-      }
-
-instance ModifyWith BuildInfo where
-  modifyWith metadata buildInfo =
-    buildInfo
-      { otherModules = [],
-        targetBuildDepends = filter is_base buildInfo.targetBuildDepends
-      }
-    where
-      is_base = ("base" ==) . unPackageName . depPkgName
-
--}
+convertString :: Text -> ByteString -> ByteString
+convertString r s = case token `stripPrefix` rest of
+  Just suffix -> concat [prefix, encodeUtf8 r, suffix]
+  Nothing -> s
+  where
+    (prefix, rest) = token `breakSubstring` s
+    token = "initialise"
