022e23927d7963ecb8901521a276f6b99f947acc
Alex Brandt <alunduil@gmail.com>
2023-12-12 18:03:17 +0000

    Replace uses of String with Text.

--- lib/initialise/Git.hs
+++ lib/initialise/Git.hs
@@ -3,5 +3,5 @@ module Git (config) where
 import Data.Text (Text, pack)
 import System.Process (readProcess)
 
-config :: String -> IO Text
+config :: Text -> IO Text
 config key = pack . head . lines <$> readProcess "git" ["config", key] ""
