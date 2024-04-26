1e17a6e9c710e86ac23ec6c229f56c0cb65b4295
Alex Brandt <alunduil@gmail.com>
2024-04-26 20:08:32 +0000

    Use NonEmpty for Git command output parsing.

--- lib/initialise/Git.hs
+++ lib/initialise/Git.hs
@@ -4,11 +4,20 @@ module Git
   )
 where
 
+import Data.List.NonEmpty (NonEmpty ((:|)), nonEmpty)
 import Data.Text (Text, pack, unpack)
 import System.Process (readProcess)
 
 config :: Text -> IO Text
-config key = pack . head . lines <$> readProcess "git" ["config", unpack key] ""
+config key = do
+  output <- nonEmpty . lines <$> readProcess "git" ["config", unpack key] []
+  case output of
+    Just (x :| _) -> pure $ pack x
+    Nothing -> fail $ "`git config " <> unpack key <> "` returned no output"
 
 root :: IO Text
-root = pack . head . lines <$> readProcess "git" ["rev-parse", "--show-toplevel"] ""
+root = do
+  output <- nonEmpty . lines <$> readProcess "git" ["rev-parse", "--show-toplevel"] ""
+  case output of
+    Just (x :| _) -> pure $ pack x
+    Nothing -> fail "`git rev-parse --show-toplevel` returned no output"
