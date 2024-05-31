67bba3dc1d3f6ae7594f9895df433f3c7c6231f2
Alex Brandt <alunduil@gmail.com>
2024-05-31 16:45:58 +0000

    Switch Configuration to ApplicativeDo
    This is based on excellent guidance from https://www.haskellforall.com/2024/05/prefer-do-notation-over-applicative.html

--- lib/initialise/Configuration.hs
+++ lib/initialise/Configuration.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE ApplicativeDo #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
 
@@ -15,9 +16,7 @@ import Defaults (Defaults (..), dCabalName, dHomePage, dName)
 import Distribution.SPDX.LicenseId (LicenseId (Unlicense))
 import Network.URI (URI, parseURI)
 import Options.Applicative
-  ( HasValue,
-    Mod,
-    Parser,
+  ( Parser,
     auto,
     help,
     hidden,
@@ -44,97 +43,91 @@ data Configuration = Configuration
   }
 
 parser :: Defaults -> Parser Configuration
-parser ds@(Defaults {..}) =
-  Configuration
-    <$> name
-    <*> cabalName
-    <*> homepage
-    <*> author
-    <*> maintainer
-    <*> licence
-    <*> path
-    <*> year
-    <*> verbosity
+parser ds@(Defaults {..}) = do
+  name <-
+    strOption
+      ( long "name"
+          <> help "Name of the new project."
+          <> metavar
+            "NAME"
+          <> maybeDefault (dName ds)
+      )
+  cabalName <-
+    strOption
+      ( long "cabal-name"
+          <> help "Name to use in cabal."
+          <> metavar "CABAL_NAME"
+          <> maybeDefault (dCabalName ds)
+      )
+  homepage <-
+    option
+      (maybeReader parseURI)
+      ( long "homepage"
+          <> help
+            "Homepage of the new project."
+          <> metavar "URL"
+          <> maybeDefault (dHomePage ds)
+      )
+  author <-
+    strOption
+      ( long "author"
+          <> help "Name of the author of the project."
+          <> metavar "AUTHOR"
+          <> value dAuthor
+          <> showDefault
+      )
+  maintainer <-
+    strOption
+      ( long "maintainer"
+          <> help
+            "Email of the maintainer of the project."
+          <> metavar "MAINTAINER"
+          <> value dMaintainer
+          <> showDefault
+      )
+  licence <-
+    option
+      auto
+      ( long "licence"
+          <> help "Licence of the project."
+          <> value Unlicense
+          <> showDefault
+          <> metavar "LICENCE"
+      )
+  path <-
+    option
+      auto
+      ( long "path"
+          <> help "Project path.  Only used for testing."
+          <> value dPath
+          <> showDefault
+          <> metavar "PATH"
+          <> hidden
+          <> internal
+      )
+  year <-
+    option
+      auto
+      ( long "year"
+          <> help "Copyright year.  Only used for testing."
+          <> value dYear
+          <> showDefault
+          <> metavar "YEAR"
+          <> hidden
+          <> internal
+      )
+  verbosity <-
+    option
+      auto
+      ( long "verbosity"
+          <> help
+            "Verbosity of information printed to stderr."
+          <> value LevelWarn
+          <> showDefault
+          <> metavar "VERBOSITY"
+      )
+  pure Configuration {..}
   where
-    name =
-      strOption
-        ( long "name"
-            <> help "Name of the new project."
-            <> metavar "NAME"
-            <> maybeDefault (dName ds)
-        )
-    cabalName =
-      strOption
-        ( long "cabal-name"
-            <> help "Name to use in cabal."
-            <> metavar "CABAL_NAME"
-            <> maybeDefault (dCabalName ds)
-        )
-    homepage =
-      option
-        (maybeReader parseURI)
-        ( long "homepage"
-            <> help "Homepage of the new project."
-            <> metavar "URL"
-            <> maybeDefault (dHomePage ds)
-        )
-    author =
-      strOption
-        ( long "author"
-            <> help "Name of the author of the project."
-            <> metavar "AUTHOR"
-            <> value dAuthor
-            <> showDefault
-        )
-    maintainer =
-      strOption
-        ( long "maintainer"
-            <> help "Email of the maintainer of the project."
-            <> metavar "MAINTAINER"
-            <> value dMaintainer
-            <> showDefault
-        )
-    licence =
-      option
-        auto
-        ( long "licence"
-            <> help "Licence of the project."
-            <> value Unlicense
-            <> showDefault
-            <> metavar "LICENCE"
-        )
-    path =
-      option
-        auto
-        ( long "path"
-            <> help "Project path.  Only used for testing."
-            <> value dPath
-            <> showDefault
-            <> metavar "PATH"
-            <> hidden
-            <> internal
-        )
-    year =
-      option
-        auto
-        ( long "year"
-            <> help "Copyright year.  Only used for testing."
-            <> value dYear
-            <> showDefault
-            <> metavar "YEAR"
-            <> hidden
-            <> internal
-        )
-    verbosity =
-      option
-        auto
-        ( long "verbosity"
-            <> help "Verbosity of information printed to stderr."
-            <> value LevelWarn
-            <> showDefault
-            <> metavar "VERBOSITY"
-        )
-
-maybeDefault :: (HasValue f, Show a) => Maybe a -> Mod f a
-maybeDefault (Just a) = value a <> showDefault
-maybeDefault Nothing = mempty
+    -- maybeDefault :: (HasValue f, Show a) => Maybe a -> Mod f a
+    maybeDefault (Just a) = value a <> showDefault
+    maybeDefault Nothing = mempty
