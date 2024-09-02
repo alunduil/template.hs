af6e902f1a3e93640df397ca39bdc25938b983ab
Alex Brandt <alunduil@gmail.com>
2024-09-02 07:58:02 +0000

    Remove Git.root and use gitlib.

--- lib/initialise/Git.hs
+++ lib/initialise/Git.hs
@@ -1,6 +1,5 @@
 module Git
   ( config,
-    root,
   )
 where
 
@@ -14,10 +13,3 @@ config key = do
   case output of
     Just (x :| _) -> pure $ pack x
     Nothing -> fail $ "`git config " <> unpack key <> "` returned no output"
-
-root :: IO Text
-root = do
-  output <- nonEmpty . lines <$> readProcess "git" ["rev-parse", "--show-toplevel"] ""
-  case output of
-    Just (x :| _) -> pure $ pack x
-    Nothing -> fail "`git rev-parse --show-toplevel` returned no output"
